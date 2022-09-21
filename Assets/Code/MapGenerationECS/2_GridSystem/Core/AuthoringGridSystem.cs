533df729c1b837d08a7b525e9532db5051c654ba
Florian <florian.duruz@gmail.com>
2022-09-21 20:50:08 +0200

    try fixedList => massive Lag in editor

--- Assets/Code/MapGenerationECS/2_GridSystem/Core/AuthoringGridSystem.cs
+++ Assets/Code/MapGenerationECS/2_GridSystem/Core/AuthoringGridSystem.cs
@@ -7,6 +7,8 @@ using Unity.Mathematics;
 using UnityEngine;
 using Unity.Entities;
 
+using static Unity.Collections.Allocator;
+using static Unity.Collections.NativeArrayOptions;
 using static UnityEngine.Mesh;
 using static KWZTerrainECS.Utilities;
 using static Unity.Mathematics.math;
@@ -21,8 +23,9 @@ namespace KWZTerrainECS
     public partial class AuthoringGridSystem : MonoBehaviour, IConvertGameObjectToEntity
     {
         public EntityManager entityManager { get; private set; }
-        
+
         private bool isActive;
+        
         private AuthoringKzwTerrain terrain;
         private TerrainSettings TerrainSettings;
 
@@ -41,37 +44,34 @@ namespace KWZTerrainECS
             if (!isActive) return;
             BlobAssetReference<GridCells> blob = CreateGridCells(TerrainSettings);
             dstManager.AddComponentData(entity, new BlobCells(){ Blob = blob });
+            //MASSIVE LAG! => need to use blob
+/*
+            DynamicBuffer<ChunkNodeGrid> buffer = dstManager.AddBuffer<ChunkNodeGrid>(entity);
+            buffer.EnsureCapacity(TerrainSettings.ChunksCount);
+            
+            using NativeArray<int> quadsIndex = GetQuadsIndexOrderedByChunk();
+            int numCellInChunk = TerrainSettings.ChunkQuadsCount;
+            for (int i = 0; i < TerrainSettings.ChunksCount; i++)
+            {
+                ChunkNode node = new (quadsIndex.Slice(i * numCellInChunk, numCellInChunk), TerrainSettings.ChunkQuadsPerLine);
+                buffer.Add(node);
+            }
+            */
         }
 
         private BlobAssetReference<GridCells> CreateGridCells(TerrainSettings terrainSetting)
         {
             BlobBuilder builder = new BlobBuilder(Allocator.Temp);
-            ref GridCells gridCells = ref builder.ConstructRoot<GridCells>();
-            /*
-            BlobBuilderArray<Cell> arrayBuilder = builder.Allocate(ref gridCells.Cells, terrainSetting.MapQuadCount);
             
-            using MeshDataArray meshDataArray = AcquireReadOnlyMeshData(terrain.chunks.GetMeshesComponent());
-            using NativeArray<float3> verticesNtv = GetOrderedVertices(terrain.chunks, terrainSetting, meshDataArray);
-
-            NativeArray<float3> cellVertices = new (4, Allocator.Temp, NativeArrayOptions.UninitializedMemory);
-            for (int cellIndex = 0; cellIndex < arrayBuilder.Length; cellIndex++)
-            {
-                (int x, int y) = GetXY(cellIndex, terrainSetting.NumQuadX);
-                for (int vertexIndex = 0; vertexIndex < 4; vertexIndex++)
-                {
-                    (int xV, int yV) = GetXY(vertexIndex, 2);
-                    int index = mad(y + yV, terrainSetting.NumVerticesX, x + xV);
-                    cellVertices[vertexIndex] = verticesNtv[index];
-                }
-                arrayBuilder[cellIndex] = new Cell(terrainSetting ,x, y, cellVertices);
-            }
-            */
+            ref GridCells gridCells = ref builder.ConstructRoot<GridCells>();
             BlobBuilderArray<Cell> arrayBuilder = ConstructGridArray(ref gridCells);
             
             BlobAssetReference<GridCells> result = builder.CreateBlobAssetReference<GridCells>(Allocator.Persistent);
             builder.Dispose();
-            return result;
-
+            return result; 
+            
+           // INNER METHODS : GET CHUNK POSITIONS
+           //==========================================================================================================
             BlobBuilderArray<Cell> ConstructGridArray(ref GridCells gridCells)
             {
                 BlobBuilderArray<Cell> arrayBuilder = builder.Allocate(ref gridCells.Cells, terrainSetting.MapQuadCount);
@@ -97,10 +97,10 @@ namespace KWZTerrainECS
 
         private NativeArray<float3> GetOrderedVertices(GameObject[] chunks, TerrainSettings terrainSetting, MeshDataArray meshDataArray)
         {
-            NativeArray<float3> verticesNtv = new(terrainSetting.MapVerticesCount, Allocator.TempJob, NativeArrayOptions.UninitializedMemory);
+            NativeArray<float3> verticesNtv = new(terrainSetting.MapVerticesCount, TempJob, UninitializedMemory);
             NativeArray<float3> chunkPosition = GetChunkPosition(chunks.Length);
             
-            NativeArray<JobHandle> jobHandles = new(chunks.Length, Allocator.Temp, NativeArrayOptions.UninitializedMemory);
+            NativeArray<JobHandle> jobHandles = new(chunks.Length, Temp, UninitializedMemory);
             for (int chunkIndex = 0; chunkIndex < chunks.Length; chunkIndex++)
             {
                 int2 chunkCoord = GetXY2(chunkIndex, terrainSetting.NumChunkWidth);
@@ -169,6 +169,59 @@ namespace KWZTerrainECS
                 int y = index / ChunkNumVertexPerLine;
                 int x = index - (y * ChunkNumVertexPerLine);
                 return new int2(x, y);
+            }
+        }
+        
+        public NativeArray<int> GetQuadsIndexOrderedByChunk()
+        {
+            NativeArray<int> nativeOrderedIndices = new (TerrainSettings.MapQuadCount, TempJob, UninitializedMemory);
+
+            JOrderArrayIndexByChunkIndex job = new()
+            {
+                CellSize = 1,
+                ChunkSize = TerrainSettings.ChunkQuadsPerLine,
+                NumCellX = TerrainSettings.NumQuadX,
+                NumChunkX = TerrainSettings.NumChunkWidth,
+                SortedArray = nativeOrderedIndices
+            };
+            JobHandle jobHandle = job.ScheduleParallel(TerrainSettings.MapQuadCount, JobWorkerCount - 1, default);
+            jobHandle.Complete();
+            /*
+            for (int i = 0; i < TerrainSettings.ChunksCount; i++)
+            {
+                chunkCells.Add(i, nativeOrderedIndices.Slice(i * gridData.TotalCellInChunk, gridData.TotalCellInChunk).ToArray());
+            }
+            return chunkCells;
+            */
+            return nativeOrderedIndices;
+        }
+        
+        [BurstCompile]
+        public struct JOrderArrayIndexByChunkIndex : IJobFor
+        {
+            [ReadOnly] public int CellSize;
+            [ReadOnly] public int ChunkSize;
+            [ReadOnly] public int NumCellX;
+            [ReadOnly] public int NumChunkX;
+
+            //[ReadOnly, NativeDisableParallelForRestriction]  public NativeArray<int> UnsortedArray;
+            [WriteOnly, NativeDisableParallelForRestriction] public NativeArray<int> SortedArray;
+
+            public void Execute(int index)
+            {
+                int2 cellCoord = GetXY2(index, NumCellX);
+                
+                float ratio = CellSize / (float)ChunkSize; //CAREFULL! NOT ChunkCellWidth but Cell compare to Chunk!
+                int2 chunkCoord = (int2)floor((float2)cellCoord * ratio);
+                int2 coordInChunk = cellCoord - (chunkCoord * ChunkSize);
+
+                int indexCellInChunk = mad(coordInChunk.y, ChunkSize,coordInChunk.x );
+                int chunkIndex =  mad(chunkCoord.y, NumChunkX, chunkCoord.x);
+                int totalCellInChunk = ChunkSize * ChunkSize;
+                
+                int indexFinal = mad(chunkIndex, totalCellInChunk, indexCellInChunk);
+
+                SortedArray[indexFinal] = index;
             }
         }
     }
--- Assets/Code/MapGenerationECS/2_GridSystem/Core/Cell/ChunkGrid.cs
+++ Assets/Code/MapGenerationECS/2_GridSystem/Core/Cell/ChunkGrid.cs
@@ -1,19 +1,79 @@
 using System;
 using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
 using Unity.Entities;
 using Unity.Mathematics;
 
 namespace KWZTerrainECS
 {
-    public struct ChunkNode : IComponentData
+    public struct ChunkNodeGrid : IBufferElementData
     {
-        public bool4 AdjacentChunks;
-        private FixedList64Bytes<DoorSide> doorSides;
-        //public DoorSide this[int index] => doorSides[index];
+        public ChunkNode Value;
+        
+        public static implicit operator ChunkNodeGrid(ChunkNode e)
+        {
+            return new ChunkNodeGrid {Value = e};
+        }
+    }
+    
+    public struct ChunkNode
+    {
+        //public bool4 AdjacentChunks;
+
+        public FixedList4096Bytes<int> CellsIndex;
+        //FixedList264 dont exist...
+        /*
+        public FixedList512Bytes<int> TopGateWay;
+        public FixedList512Bytes<int> RightGateWay;
+        public FixedList512Bytes<int> BottomGateWay;
+        public FixedList512Bytes<int> LeftGateWay;
+*/
+        public ChunkNode(NativeSlice<int> indices, int chunkQuadPerLine)
+        {
+            CellsIndex = new FixedList4096Bytes<int>();
+            /*
+            TopGateWay    = new FixedList512Bytes<int>();
+            RightGateWay  = new FixedList512Bytes<int>();
+            BottomGateWay = new FixedList512Bytes<int>();
+            LeftGateWay   = new FixedList512Bytes<int>();
+*/
+            for (int i = 0; i < indices.Length; i++)
+            {
+                CellsIndex.Add(indices[i]);
+            }
+            /*
+            unsafe
+            {
+                CellsIndex.AddRange(indices.GetUnsafeReadOnlyPtr(), indices.Length);
+            }
+            */
+            /*
+            for (int i = 0; i < indices.Length; i++)
+            {
+                int2 coord = Utilities.GetXY2(i, chunkQuadPerLine);
+                if (coord.y == 0)
+                {
+                    BottomGateWay.Add(CellsIndex[i]);
+                }
+                if (coord.y == chunkQuadPerLine - 1)
+                {
+                    TopGateWay.Add(CellsIndex[i]);
+                }
+                if (coord.x == 0)
+                {
+                    LeftGateWay.Add(CellsIndex[i]);
+                }
+                if (coord.x == chunkQuadPerLine - 1)
+                {
+                    RightGateWay.Add(CellsIndex[i]);
+                }
+            }
+            */
+        }
     }
 
-    public struct DoorSide
+    public struct GateWay
     {
-        private FixedList32Bytes<Int16> test1;
+        public FixedList32Bytes<short> test1;
     }
 }
