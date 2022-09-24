2972f4dc8c7d356598f226f4cb71cd58320ff16f
Florian <florian.duruz@gmail.com>
2022-09-24 19:14:52 +0200

    test new structure for gateways

--- Assets/Code/MapGenerationECS/2_GridSystem/Core/Cell/ChunkGrid.cs
+++ Assets/Code/MapGenerationECS/2_GridSystem/Core/Cell/ChunkGrid.cs
@@ -3,6 +3,9 @@ using Unity.Collections;
 using Unity.Collections.LowLevel.Unsafe;
 using Unity.Entities;
 using Unity.Mathematics;
+using static KWZTerrainECS.Sides;
+
+using static Unity.Mathematics.math;
 
 namespace KWZTerrainECS
 {
@@ -16,64 +19,119 @@ namespace KWZTerrainECS
         }
     }
     
+    //==========================================================
+
+    public enum Sides : int
+    {
+        Top    = 0,
+        Right  = 1,
+        Bottom = 2,
+        Left   = 3
+    }
+    
+    public struct ChunkNodes : IComponentData
+    {
+        public BlobAssetReference<ChunkNodesPool> Blob;
+    }
+    
+    public struct ChunkNodesPool
+    {
+        public BlobArray<ChunkNode> Blob;
+    }
+    
     public struct ChunkNode
     {
         //public bool4 AdjacentChunks;
-
-        public FixedList4096Bytes<int> CellsIndex;
-        //FixedList264 dont exist...
-        /*
-        public FixedList512Bytes<int> TopGateWay;
-        public FixedList512Bytes<int> RightGateWay;
-        public FixedList512Bytes<int> BottomGateWay;
-        public FixedList512Bytes<int> LeftGateWay;
-*/
-        public ChunkNode(NativeSlice<int> indices, int chunkQuadPerLine)
+        
+        public BlobArray<GateWay> TopGatePairIndices;
+        public BlobArray<GateWay> RightGatePairIndices;
+        public BlobArray<GateWay> BottomGatePairIndices;
+        public BlobArray<GateWay> LeftGatePairIndices;
+        
+        public static ref ChunkNode Build(int index, int numChunkPerLine, int2 numChunksXY, int chunkSize)
         {
-            CellsIndex = new FixedList4096Bytes<int>();
-            /*
-            TopGateWay    = new FixedList512Bytes<int>();
-            RightGateWay  = new FixedList512Bytes<int>();
-            BottomGateWay = new FixedList512Bytes<int>();
-            LeftGateWay   = new FixedList512Bytes<int>();
-*/
-            for (int i = 0; i < indices.Length; i++)
+            BlobBuilder builder = new BlobBuilder(Allocator.Temp);
+            ref ChunkNode chunkNode = ref builder.ConstructRoot<ChunkNode>();
+            
+            int2 chunkCoord = Utilities.GetXY2(index, numChunkPerLine);
+            
+            //int numSide = 4;
+            //numSide -= select(0, 1, chunkCoord.y == 0 || chunkCoord.y == numChunksXY.y - 1);
+            //numSide -= select(0, 1, chunkCoord.x == 0 || chunkCoord.x == numChunksXY.x - 1);
+
+            int2 terrainSize = numChunksXY * chunkSize;
+            for (int i = 0; i < 4; i++)
             {
-                CellsIndex.Add(indices[i]);
+                //BlobBuilderArray<GateWay> arrayBuilder = builder.Allocate(ref chunkNode.GatePairIndices, numSide * chunkSize);
+                Sides side = (Sides)i;
+
+                if (side == Bottom && chunkCoord.y == 0)                 continue;
+                if (side == Top    && chunkCoord.y == numChunksXY.y - 1) continue;
+                if (side == Left   && chunkCoord.x == 0)                 continue;
+                if (side == Right  && chunkCoord.x == numChunksXY.x - 1) continue;
+
+                NativeArray<GateWay> gateWays = GetGateWaysIndices(side);
+
+                switch (side)
+                {
+                    case Top:
+                        builder.Allocate(ref chunkNode.TopGatePairIndices, gateWays.Length);
+                        builder.Construct(ref chunkNode.TopGatePairIndices, gateWays.ToArray());
+                        break;
+                    case Right:
+                        builder.Allocate(ref chunkNode.RightGatePairIndices, gateWays.Length);
+                        builder.Construct(ref chunkNode.TopGatePairIndices, gateWays.ToArray());
+                        break;
+                    case Bottom:
+                        builder.Allocate(ref chunkNode.BottomGatePairIndices, gateWays.Length);
+                        builder.Construct(ref chunkNode.TopGatePairIndices, gateWays.ToArray());
+                        break;
+                    case Left:
+                        builder.Allocate(ref chunkNode.LeftGatePairIndices, gateWays.Length);
+                        builder.Construct(ref chunkNode.TopGatePairIndices, gateWays.ToArray());
+                        break;
+                }
             }
-            /*
-            unsafe
+            builder.Dispose();
+            
+            return ref chunkNode;
+
+            //==================================================
+            NativeArray<GateWay> GetGateWaysIndices(Sides side)
             {
-                CellsIndex.AddRange(indices.GetUnsafeReadOnlyPtr(), indices.Length);
+                NativeArray<GateWay> gates = new (chunkSize, Allocator.Temp);
+                int2 offsetChunk = chunkSize * chunkCoord;
+
+                bool2 isYOffset = new(side == Top, side == Bottom);
+                bool2 isXOffset = new(side == Left, side == Right);
+
+                for (int i = 0; i < chunkSize; i++)
+                {
+                    int2 gateCoord = Utilities.GetXY2(i, chunkSize) + offsetChunk;
+                    int offsetY = select( select(1,-1,isYOffset.y), 0,!any(isYOffset) );
+                    int offsetX = select( select(1,-1,isXOffset.x), 0,!any(isXOffset) );
+                    
+                    int gateIndex = gateCoord.y * terrainSize.x + gateCoord.x;
+                    int adjGateIndex = (gateCoord.y + offsetY) * terrainSize.x + (gateCoord.x + offsetX);
+                    gates[i] = new GateWay(gateIndex, adjGateIndex);
+                }
+
+                return gates;
             }
-            */
-            /*
-            for (int i = 0; i < indices.Length; i++)
-            {
-                int2 coord = Utilities.GetXY2(i, chunkQuadPerLine);
-                if (coord.y == 0)
-                {
-                    BottomGateWay.Add(CellsIndex[i]);
-                }
-                if (coord.y == chunkQuadPerLine - 1)
-                {
-                    TopGateWay.Add(CellsIndex[i]);
-                }
-                if (coord.x == 0)
-                {
-                    LeftGateWay.Add(CellsIndex[i]);
-                }
-                if (coord.x == chunkQuadPerLine - 1)
-                {
-                    RightGateWay.Add(CellsIndex[i]);
-                }
-            }
-            */
         }
     }
 
-    public struct GateWay
+    //==========================================================
+    
+    public readonly struct GateWay
     {
-        public FixedList32Bytes<short> test1;
+        public readonly int Index;
+        public readonly int IndexAdjacent;
+
+        public GateWay(int index, int indexAdj)
+        {
+            Index = index;
+            IndexAdjacent = indexAdj;
+        }
     }
 }
